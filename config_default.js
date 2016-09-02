cea1e7e6e2e0e22f89ea0a5a040321ba1adeeeef
gurdzain <gurdzain@163.com>
2016-09-02 10:19:39 +0800

    test

--- config_default.js
+++ config_default.js
@@ -1,10 +1,10 @@
 var config = {
     debug: true,
-    db: 'mongodb://myblog:ilovezy0622@123.207.142.44:26023/myblog',
+    db: '',
 
     redis:{
         host: '127.0.0.1',
-        port: 32791,
+        port: ,
         db: 0
     }
 };
